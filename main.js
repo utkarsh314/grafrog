233c67fc171f459c36dda5438e6ce52cc983bcfc
Utkarsh Sharma <uuutkarsh239@gmail.com>
2023-05-01 19:43:29 +0530

    add translation and rotation

--- main.js
+++ main.js
@@ -1,28 +1,68 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene(); // create a new scene
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000); // add camera
+// create a new scene
+const scene = new THREE.Scene(); 
 
-const renderer = new THREE.WebGLRenderer(); // create a renderer 
-renderer.setSize(window.innerWidth, window.innerHeight); // and set its size to fit your window
+// add a camera and adjust its position
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 10);
 
-document.body.appendChild(renderer.domElement); // add a <canvas> tag to the HTML
-camera.position.z = 5; // adjust camera position
+// create a renderer and set its size to fit the device window
+const renderer = new THREE.WebGLRenderer(); 
+renderer.setSize(window.innerWidth, window.innerHeight); 
+
+// add a <canvas> tag to the HTML
+document.body.appendChild(renderer.domElement); 
+
+//add lighting
+var ambient = new THREE.AmbientLight(0xffffff, 0.5);  // ambient light source
+scene.add(ambient);
+var point = new THREE.PointLight(0xffffff, 0.8);      // point light source
+scene.add(point);
+
+// load the frog model
+var frog;
 
 const loader = new GLTFLoader();
-
-loader.load('assets/frog.glb',
+loader.load(
+	'assets/frog.glb',
 	function(gltf){	
-		gltf.scene.scale.set(.001*gltf.scene.scale.x, .001*gltf.scene.scale.y, .001 * gltf.scene.scale.z);
+		frog = gltf.scene;
 		scene.add(gltf.scene);
+		console.log(scene.children);
 	},
 	undefined,
 	function(error){
 		console.error(error);
-	});
+	}
+);
 
-function animate() {
+const xSpeed = 10;
+const ySpeed = 10;
+const xAngle = 0.1;
+const yAngle = 0.1;
+const smoothness= 0.01;
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.key;
+	const targetPosition = frog.position.clone();
+	const targetRotation = frog.rotation.clone();
+
+    if(!event.shiftKey && keyCode == "ArrowUp") targetPosition.y += ySpeed; 
+	else if(!event.shiftKey && keyCode == "ArrowDown") targetPosition.y -= ySpeed;
+    else if(!event.shiftKey && keyCode == "ArrowLeft") targetPosition.x -= xSpeed;
+    else if(!event.shiftKey && keyCode == "ArrowRight") targetPosition.x += xSpeed;
+	else if(event.shiftKey && keyCode == "ArrowUp") frog.rotation.x -= yAngle; 
+	else if(event.shiftKey && keyCode == "ArrowDown") frog.rotation.x += yAngle;
+    else if(event.shiftKey && keyCode == "ArrowLeft") frog.rotation.z -= xAngle;
+    else if(event.shiftKey && keyCode == "ArrowRight") frog.rotation.z += xAngle;
+
+	if(!event.shiftKey) frog.position.lerp(targetPosition, smoothness);
+};
+ 
+function animate(){
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
