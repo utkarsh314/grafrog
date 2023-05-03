c71cabd936538f5f6d33a996f6dabb132a127366
Utkarsh Sharma <uuutkarsh239@gmail.com>
2023-05-04 01:30:12 +0530

    final commit

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xa8def0);
 
 // add a camera and adjust its position
-
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.set(0, 3, 10);
 
@@ -17,10 +15,11 @@ const renderer = new THREE.WebGLRenderer({
 	antialias: true
 }); 
 renderer.setSize(window.innerWidth, window.innerHeight); 
-
+// enable shadows
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.BasicShadowMap;
 
+// add an orbit controller
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.update();
 
@@ -31,11 +30,8 @@ document.body.appendChild(renderer.domElement);
 var ambient = new THREE.AmbientLight(0xffffff, 0.5);  // ambient light source
 //ambient.castShadow=true;
 scene.add(ambient);
-var point = new THREE.PointLight(0xffffff, 0.8);      // point light source
-point.castShadow=true;
-scene.add(point);
 
-var dLight=new THREE.DirectionalLight(0xffffff,1);
+var dLight=new THREE.DirectionalLight(0xffffff,1); // directional light source
 dLight.position.set(0,300,0)
 scene.add(dLight);
 dLight.castShadow=true;
@@ -43,13 +39,13 @@ dLight.castShadow=true;
 // load the frog model
 var frog;
 
-//Koad Texture
-const textureLoader=new THREE.TextureLoader();
-const frogTexture=textureLoader.load('assets/skin.jpg');
-const mat=new THREE.MeshPhongMaterial();
-mat.map=frogTexture;
+// //Load Texture
+// const textureLoader=new THREE.TextureLoader();
+// const frogTexture=textureLoader.load('assets/skin.jpg');
+// const mat=new THREE.MeshPhongMaterial();
+// mat.map=frogTexture;
 
-
+// add the ground plane
 const geometryP = new THREE.PlaneGeometry(50, 50);
 const materialP = new THREE.MeshStandardMaterial({color:0xffffff})
 const plane = new THREE.Mesh(geometryP, materialP);
@@ -105,8 +101,11 @@ var lTibia; var lFoot;
 
 //ArmValues
 var Humerus;
+
 //Skull
 var skull;
+
+// loading the frog model
 const loader = new GLTFLoader();
 loader.load(
 	'assets/frog6.glb',
@@ -120,15 +119,16 @@ loader.load(
 		});
         frog.position.y+=2;
 		frog.rotation.y+=Math.PI/2;
+		frog.getObjectByName('skull').rotation.z+=0.2;
         frog.castShadow=true;
         frog.receiveShadow=true;
-		//Store right Foot values
-		rTibia=frog.getObjectByName('righttibia').rotation.z; rFoot=frog.getObjectByName('rightfoot').rotation.z;
-		//Store left Foot Values
-		lTibia=frog.getObjectByName('lefttibia').rotation.z; lFoot=frog.getObjectByName('leftfoot').rotation.z;
-		//Humerus
-		Humerus=frog.getObjectByName('righthumerus').rotation.z; 
 
+		// store the original positions of the frog's limbs
+		rTibia=frog.getObjectByName('righttibia').rotation.z; 
+		rFoot=frog.getObjectByName('rightfoot').rotation.z;
+		lTibia=frog.getObjectByName('lefttibia').rotation.z;
+		lFoot=frog.getObjectByName('leftfoot').rotation.z;
+		Humerus=frog.getObjectByName('righthumerus').rotation.z; 
 		skull=frog.getObjectByName('skull').rotation.x;
 
 		scene.add(frog);
@@ -140,13 +140,14 @@ loader.load(
 	}
 );
 
-
+// set up some constants for movement
 const xSpeed = 10;
 const ySpeed = 10;
 const xAngle = 0.1;
 const yAngle = 0.1;
 const smoothness= 0.01;
 
+//add an event listener and trigger movements for respective keypresses
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
     var keyCode = event.key;
@@ -168,7 +169,6 @@ function onDocumentKeyDown(event) {
 			frog.getObjectByName('lefttibia').rotation.z-=0.2;
 			frog.getObjectByName('leftfoot').rotation.z-=1;
 		}
-		
 	}
 	else if (keyCode=="w") {
 		if(frog.getObjectByName('righthumerus').rotation.z==Humerus){
@@ -187,20 +187,18 @@ function onDocumentKeyDown(event) {
 		}
 	}
 
-
-	if(!event.shiftKey) frog.position.lerp(targetPosition, smoothness);
+	if(!event.shiftKey) frog.position.lerp(targetPosition, smoothness); // make translations smooth using linear interpolations
 };
  
+// bring the frog back to its original position on key up
 document.addEventListener("keyup", onDocumentKeyUp, false);
 function onDocumentKeyUp(event) {
     var keyCode = event.key;
 	const targetPosition = frog.position.clone();
 	const targetRotation = frog.rotation.clone();
 	if (keyCode=="s") {
-
 		frog.getObjectByName('righttibia').rotation.z=rTibia;
 		frog.getObjectByName('rightfoot').rotation.z=rFoot;
-
 		frog.getObjectByName('lefttibia').rotation.z=lTibia;
 		frog.getObjectByName('leftfoot').rotation.z=lFoot;
 	}
@@ -211,10 +209,9 @@ function onDocumentKeyUp(event) {
 	else if(keyCode=="a"||keyCode=="d"){
 		frog.getObjectByName('skull').rotation.x=skull;
 	}
-
-	if(!event.shiftKey) frog.position.lerp(targetPosition, smoothness);
 };
 
+// render loop
 function animate(){
 	requestAnimationFrame( animate );
 	controls.update();
